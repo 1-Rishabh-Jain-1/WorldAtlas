beee7c5e7405c256b82f058f0c482115935ff9f1
Rishabh <rarishabhjain@gmail.com>
2025-02-02 12:34:40 +0530

    Added error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { About } from "./pages/About";
 import { Country } from "./pages/Country";
 import { Contact } from "./pages/Contact";
 import { AppLayout } from "./components/Layout/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
 
 const router = createBrowserRouter(
   [
     {
       path: "/",
       element: <AppLayout />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "/",
