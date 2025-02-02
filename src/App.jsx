4c16b3319cc56a7c62aadbca2ff1f0c07bd33de4
Rishabh <rarishabhjain@gmail.com>
2025-02-02 11:15:01 +0530

    Implemented router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { Home} from "./pages/Home";
+import { About} from "./pages/About";
+import { Country} from "./pages/Country";
+import { Contact} from "./pages/Contact";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "about",
+      element: <About />,
+    },
+    {
+      path: "country",
+      element: <Country />,
+    },
+    {
+      path: "contact",
+      element: <Contact />,
+    },
+  ]
+);
 
 const App = () => {
-  return <h1>My React App</h1>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
